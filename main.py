da4fa6432e7b57ed4863eb025eff17ca2c3c0982
JASSJayasinghe2021 <vovolabs@gmail.com>
2024-11-03 02:09:27 +0900

    second comment

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ app = FastAPI()
 
 @app.get("/consent")
 async def root():
-    return {"message":""}
+    return {"message":"Hi"}
+
+@app.get("/districts")
+async def root():
+    return {
+            "districts":["Colombo", "Badulla"],
+        }
+    
+
+@app.get("/ds")
+async def root():
+    return {
+            "ds":["Badulla", "Passara"],
+        }
+
+@app.get("/gn")
+async def root():
+    return {
+            "gs":["Badulla South", "Badulla North"],
+        }
